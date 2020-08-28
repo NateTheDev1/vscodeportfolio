3d8250aeffbc9bfa57c53c73ffe920a9ce31c3f5
NateTheDev1 <nthnlrichards@gmail.com>
2020-08-28 01:15:00 -0400

    To Test Initial Deployment

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
-};
+// Root CSS
+import "./css/normalize.css";
+import "./css/index.css";
+
+// Root App
+import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,18 @@ module.exports = {
   module: {
     rules: [
       { test: /\.(js)$/, use: "babel-loader" },
-      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
     ],
   },
   mode: "development",
