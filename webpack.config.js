7cd0cb7a55e8de230f996eddb64b30a60a8c947a
NateTheDev1 <nthnlrichards@gmail.com>
2020-08-28 00:45:34 -0400

    Initial Project Setup

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./app/src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js",
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: "babel-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+    ],
+  },
+  mode: "development",
+  plugins: [new htmlWebpackPlugin({ template: "app/public/index.html" })],
+};
