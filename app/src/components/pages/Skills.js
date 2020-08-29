c590eb4b4de03c9d029c857e08e17123e30b59a3
NateTheDev1 <nthnlrichards@gmail.com>
2020-08-29 01:47:18 -0400

    Skills done

--- app/src/components/pages/Skills.js
+++ app/src/components/pages/Skills.js
@@ -6,7 +6,109 @@ const Skills = () => {
   return (
     <div className="sk-root">
       <CodeBar rowAmount={50} />
-      <div className="sk-content"></div>
+      <div className="sk-content">
+        <p>
+          {" "}
+          <span className="purp">import</span> React{" "}
+          <span className="purp">from</span>{" "}
+          <span className="green">{`"react"`}</span>;
+        </p>
+        <p>
+          {" "}
+          <span className="purp">import</span> SoftwareEngineer{" "}
+          <span className="purp">from</span>{" "}
+          <span className="green">{`"nathaniel-richards"`}</span>;
+        </p>
+        <div className="component" style={{ marginTop: "10%" }}>
+          <p>
+            <span className="purp">const</span>{" "}
+            <span className="blu">Skills = </span>
+            <span className="orange">()</span>
+            <span className="purp">{` =>`} </span>
+            <span className="orange">SoftwareEngineer.Create()</span>
+            <span className="purp">{` =>`} </span>
+            <span className="bracket">{` {`}</span>
+          </p>
+          <p style={{ paddingLeft: "3%" }}>
+            <span className="purp">return</span>
+            <span className="orange"> (</span>
+          </p>
+          <p style={{ paddingLeft: "8%" }}>
+            <span className="red">{`<h1>`} </span> CORE SKILLS{" "}
+            <span className="red">{`</h1>`}</span>
+          </p>
+          <p style={{ paddingLeft: "8%" }}>
+            <span className="red">{`<p>`}</span>
+          </p>
+          <ul style={{ paddingLeft: "20%" }}>
+            <li>JavaScript</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>LESS</li>
+          </ul>
+          <p style={{ paddingLeft: "8%" }}>
+            <span className="red">{`</p>`}</span>
+          </p>
+          <p style={{ paddingLeft: "8%" }}>
+            <span className="red">{`<h1>`} </span> FRONTEND TECHNOLOGIES{" "}
+            <span className="red">{`</h1>`}</span>
+          </p>
+          <p style={{ paddingLeft: "8%" }}>
+            <span className="red">{`<p>`}</span>
+          </p>
+          <ul style={{ paddingLeft: "20%" }}>
+            <li>ReactJS</li>
+            <li>Redux</li>
+            <li>Context</li>
+            <li>React Native</li>
+            <li>MaterialUI</li>
+            <li>Bootstrap & Reactstrap</li>
+            <li>Materialize</li>
+            <li>Styled-Components</li>
+            <li>Apollo Client</li>
+          </ul>
+          <p style={{ paddingLeft: "8%" }}>
+            <span className="red">{`</p>`}</span>
+          </p>
+          <p style={{ paddingLeft: "8%" }}>
+            <span className="red">{`<h1>`} </span> BACKEND TECHNOLOGIES{" "}
+            <span className="red">{`</h1>`}</span>
+          </p>
+          <p style={{ paddingLeft: "8%" }}>
+            <span className="red">{`<p>`}</span>
+          </p>
+          <ul style={{ paddingLeft: "20%" }}>
+            <li>NodeJS</li>
+            <li>Express</li>
+            <li>PostgreSQL</li>
+            <li>SQLite</li>
+            <li>GraphQL</li>
+            <li>Apollo Server</li>
+            <li>MongoDB</li>
+            <li>PocketDB</li>
+            <li>Firebase</li>
+          </ul>
+          <p style={{ paddingLeft: "8%" }}>
+            <span className="red">{`</p>`}</span>
+          </p>
+          <p style={{ paddingLeft: "8%" }}>
+            <span className="red">{`<h1>`} </span> TESTING{" "}
+            <span className="red">{`</h1>`}</span>
+          </p>
+          <p style={{ paddingLeft: "8%" }}>
+            <span className="red">{`<p>`}</span>
+          </p>
+          <ul style={{ paddingLeft: "20%" }}>
+            <li>Cypress.io</li>
+            <li>Jest</li>
+            <li>supertest</li>
+            <li>Enzyme</li>
+          </ul>
+          <p style={{ paddingLeft: "8%" }}>
+            <span className="red">{`</p>`}</span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- app/src/services/reducer.js
+++ app/src/services/reducer.js
@@ -2,7 +2,7 @@ import { UPDATE_PAGE, LOADING } from "./types";
 
 const initialState = {
   loading: false,
-  page: "about.md",
+  page: "skills.js",
 };
 
 export const reducer = (state = initialState, action) => {
